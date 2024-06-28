708c63b5cde5be165d4f0e10a7b35f75a89a44e0
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-28 02:32:54 +0100

    reroute

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is an api route</p>
-        </a>
       </header>
     </div>
   );
