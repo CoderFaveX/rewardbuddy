96e8a40933bd67a639ab7557aad8945bcf2e5eb6
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-29 00:45:31 +0100

    changing fend routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
-import ClaimAirtime from './web/ClaimAirtime';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import ClaimAirtime from "./web/ClaimAirtime";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Router>
     <Routes>
-      <Route exact path="/" component={App} />
-      <Route path="/claimairtime" component={ClaimAirtime} />
+      <Route path="/" element={<App />} />
+      <Route path="/claimairtime" element={<ClaimAirtime />} />
     </Routes>
   </Router>
 );
