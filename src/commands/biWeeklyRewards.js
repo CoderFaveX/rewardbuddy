dacee25b0dff74eca4b79982e090f89b99389838
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-29 10:04:47 +0100

    checking type of chatId

--- src/commands/biWeeklyRewards.js
+++ src/commands/biWeeklyRewards.js
@@ -43,8 +43,8 @@ async function distributeBiWeeklyRewards() {
       for (const user of topUsers) {
         const currentUser = User.findOne({ chatId: user.chatId });
         console.log(
-          "Chat Id: %s, User_found: %s",
-          user.chatId,
+          "Chat Id: %s[%s], User_found: %s",
+          user.chatId, type of user.chatId
           currentUser.chatId
         );
         currentUser.validToken = true;
